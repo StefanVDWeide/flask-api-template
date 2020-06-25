ccd539e0a0a7b3a5b8481f8d4e7838204bf53f43
Stefan <stefan.vander.weide@hotmail.com>
2020-06-25 18:20:10 +0200

    Added ENV config variable to tell Flask what environment is used

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    ENV = "development"
     SECRET_KEY = os.environ.get('SECRET_KEY') or "CHANGE-THIS-SECRET-KEY"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
