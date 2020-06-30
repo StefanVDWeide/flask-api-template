687422be6a10e103698c2b34110e8bcf3714d73f
Stefan <stefan.vander.weide@hotmail.com>
2020-07-01 00:37:46 +0200

    Minor clean up

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,7 +4,7 @@ from werkzeug.http import HTTP_STATUS_CODES
 
 # A catch all function which returns the error code and message back to the user
 def error_response(status_code, message=None):
-    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    payload = {"error": HTTP_STATUS_CODES.get(status_code, "Unknown error")}
     if message:
         payload["msg"] = message
     response = jsonify(payload)
