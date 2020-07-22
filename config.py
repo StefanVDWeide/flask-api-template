b1158b8e204ddb9abc069797307b3e875f41f54d
Stefan <stefan.vander.weide@hotmail.com>
2020-07-22 15:14:56 +0200

    Added support for .env and .flaskenv variables

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+# Set base directory of the app
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Load the .env and .flaskenv variables
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
-    # Set the ENV variable to "producution" when deploying to production. Ideally this is set as an environment variable
-    ENV = "development"
-    DEBUG = True
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "CHANGE-THIS-SECRET-KEY"
+    # Set the config variables
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "CHANGE-THIS-TO-A-SECRET-STRING"
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
