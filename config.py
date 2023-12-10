7413b9af6d726319bbf90b5eadba43e00b501270
gkowalc <gkowalc>
2023-12-10 18:28:16 +0100

    added postgres db setup steps

--- config.py
+++ config.py
@@ -20,7 +20,13 @@ class Config(object):
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    # psotgresql crednetials. If you want to use postgres db, uncomment the following lines and comment out  above  SQLALCHEMY_DATABASE_URI corresponding to sqlite
+    #hostname = os.getenv("DB_HOSTNAME")
+    #user = os.getenv("USERNAME_DB")
+    #database = os.getenv("DATABASE")
+    #password = os.getenv("PASSWORD")
+    #port = os.getenv("PORT")
+    #  SQLALCHEMY_DATABASE_URI = f"postgresql://{user}:{password}@{hostname}:{port}/{database}"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
